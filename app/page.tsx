8e2f20411143ae6c905c33208ae54a51597a0cb6
Kibeom Jeon <kibeom1145@gmail.com>
2024-02-05 17:27:22 +0900

    tailwind practice 2

--- app/page.tsx
+++ app/page.tsx
@@ -1,79 +1,20 @@
 
 export default function Home() {
   return (
-      <div className="bg-slate-400 px-20 py-20 grid gap-10 min-h-screen">
-        <div className="bg-white p-6 rounded-3xl shadow-xl">
-          <span className="font-semibold text-3xl">Select Item</span>
-          <div className="flex justify-between my-2">
-            <span className="text-gray-500">Grey Chair</span>
-            <span className="font-semibold">$19</span>
-          </div>
-          <div className="flex justify-between">
-            <span className="text-gray-500">Grey Chair</span>
-            <span className="font-semibold">$19</span>
-          </div>
-          <div className="flex justify-between mt-2 pt-2 border-t-2 border-dashed">
-            <span>Total</span>
-            <span className="font-semibold">$10</span>
-          </div>
-          <button className="flex justify-center mt-5 bg-blue-500 text-white p-3 text-center rounded-xl w-2/4 mx-auto
-          hover:bg-teal-500 active:bg-yellow-500">
-            Checkout
-          </button>
-        </div>
-        <div className="bg-white overflow-hidden rounded-3xl shadow-xl">
-          <div className="bg-blue-500 p-6 pb-14">
-            <span className="text-white text-2xl">Profile</span>
-          </div>
-          <div className="bg-white rounded-3xl p-6 relative -top-5">
-            <div className="flex relative -top-16 items-end justify-between">
-              <div className="flex flex-col items-center">
-                <span className="text-sm text-gray-500">Orders</span>
-                <span className="font-semibold">340</span>
-              </div>
-              <div className="h-24 w-24 bg-zinc-300 rounded-full"></div>
-              <div className="flex flex-col items-center">
-                <span className="text-sm text-gray-500">Spent</span>
-                <span className="font-semibold">$2,310</span>
-              </div>
-            </div>
-            <div className="relative flex flex-col items-center -mt-10 -mb-5">
-                <span className="text-lg font-semibold">Tony Molloy</span>
-                <span className="text-sm text-gray-500">미국</span>
-              </div>
-          </div>
-        </div>
-        <div className="bg-white p-6 rounded-3xl shadow-xl">
-          <div className="flex mb-5 justify-between items-center">
-            <span className="font-semibold text-2xl">←</span>
-            <div className="space-x-3">
-              <span className="text-lg text-yellow-400">★</span>
-              <span className="font-semibold relative -left-2">4.9</span>
-              <span className="text-lg text-red-400">♥</span>
-            </div>
-          </div>
-          <div className="bg-zinc-400 h-72 mb-5"/>
-          <div className="flex flex-col">
-            <span className="font-semibold text-xl">Swoon Lounge</span>
-            <span className="text-xs text-gray-500">Chair</span>
-            <div className="mt-3 mb-5 flex justify-between">
-              <div className="space-x-2">
-                <button className="w-5 h-5 rounded-full bg-yellow-500 focus:ring-2 ring-offset-2 ring-yellow-500 transition"></button>
-                <button className="w-5 h-5 rounded-full bg-indigo-500 focus:ring-2 ring-offset-2 ring-indigo-500 transition"></button>
-                <button className="w-5 h-5 rounded-full bg-teal-500 focus:ring-2 ring-offset-2 ring-teal-500 transition"></button>
-              </div>
-              <div className="flex items-center space-x-4">
-                <button className="p-1.5 bg-blue-200 flex justify-center items-center aspect-square w-8 font-semibold rounded-lg text-gray-500">-</button>
-                <span>1</span>
-                <button className="p-1.5 bg-blue-200 flex justify-center items-center aspect-square w-8 font-semibold rounded-lg text-gray-500">+</button>
-              </div>
-            </div>
-            <div className="flex justify-between items-center">
-              <span className="font-semibold text-2xl">$450</span>
-              <button className="bg-blue-500 p-2 text-center text-white rounded-lg py-2 px-8">Add to cart</button>
-            </div>
-          </div>
-        </div>
-      </div>
+    <form action="" className="flex flex-col space-y-2 p-5">
+      <input
+        type="text"
+        required
+        placeholder="Username"
+        className="border p-1 peer border-gray-400 rounded-md"
+      />
+      <span className="hidden peer-invalid:block text-red-500 ">
+        This input is invalid
+        </span>
+      <span className="hidden peer-valid:block text-teal-500 ">
+        Awesome Username
+      </span>
+      <input type="submit" value="Login" className=""/>
+    </form>
   );
 }
